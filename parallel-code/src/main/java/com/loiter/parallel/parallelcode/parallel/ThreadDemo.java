54e86ac948b5bcc4b2fbd765fa902d2e56a3562d
ant-loiter <13552666934@139.com>
2020-10-31 22:38:45 +0800

    no message

--- parallel-code/src/main/java/com/loiter/parallel/parallelcode/parallel/ThreadDemo.java
+++ parallel-code/src/main/java/com/loiter/parallel/parallelcode/parallel/ThreadDemo.java
@@ -11,6 +11,31 @@ public class ThreadDemo {
         Thread thread = new Thread(ThreadDemo::helloWorld);
         thread.start();
         thread.join();
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+            if (Thread.interrupted()) {
+                System.out.println("当前线程被interrupted " + Thread.interrupted());
+                System.out.println("Hello, Interrupted ");
+            }
+        });
+        thread1.start();
+
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+            thread1.interrupt();
+            if(thread1.isInterrupted()) {
+                System.out.println("thread1 is interrupted");
+            }
+            try {
+                thread1.join(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread2.start();
+
     }
 
     private static void helloWorld() {
